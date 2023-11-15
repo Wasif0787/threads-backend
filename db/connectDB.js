b7164a88d87a639ac3a2c95134366039f46d159d
Wasif0787 <wasifhussain787@gmail.com>
2023-11-15 21:49:15 +0530

    third commit

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect("mongodb+srv://wasifhussain787:wasifhussain787@cluster0.bdw80hd.mongodb.net/threads?retryWrites=true&w=majority");
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
