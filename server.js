53d4b9af1f7bd229e52dbec6f15632de7b492be7
Wasif0787 <wasifhussain787@gmail.com>
2023-11-16 13:24:57 +0530

    Added cors

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ cloudinary.config({
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
+  
+
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }))
 app.use(cookieParser());
