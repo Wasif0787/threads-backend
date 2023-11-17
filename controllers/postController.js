7850a96f80aa9def25859883af5bc423ade43c67
Wasif0787 <wasifhussain787@gmail.com>
2023-11-17 21:12:30 +0530

    almost

--- controllers/postController.js
+++ controllers/postController.js
@@ -113,7 +113,7 @@ const replyToPost = async (req, res) => {
         post.replies.push(reply);
         await post.save();
 
-        res.status(200).json(post);
+        res.status(200).json(reply);
     } catch (err) {
         res.status(500).json({ error: err.message })
         console.log(`Error in replyToPost : ${err.message}`);
